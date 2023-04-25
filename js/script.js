d2fa931bef42961c75f3c6dc6e28bbc14f299fa6
Belousov <wasik787@gmail.com>
2023-04-25 19:48:47 +0500

    feat: myIconStock

--- js/script.js
+++ js/script.js
@@ -103,7 +103,7 @@ function addMarker(data_in, icon, comment ) {
 
 
 
-function cleanLayersGroup(id) { // удалить все слои пути
+function cleanLayersGroup(id) { // удалить все слои пути id = route_0
   myLayers[id].clearLayers()
 }
 
@@ -244,6 +244,13 @@ var myIconActive = L.divIcon({
   html: '<div class="my-div-icon_inner"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="-4 0 36 36"><g fill="none" fill-rule="evenodd"><path fill="#3276c3" d="M14 0c7.732 0 14 5.641 14 12.6C28 23.963 14 36 14 36S0 24.064 0 12.6C0 5.641 6.268 0 14 0Z"/><circle cx="14" cy="14" r="7" fill="#fff" fill-rule="nonzero"/></g></svg></div>',
 });
 
+
+
+var myIconStock = L.icon({
+  iconUrl: '/img/icon.png',
+  iconSize: [64, 64],
+});
+
 //------------------------------------------------------------------------------------------
 
 // function init(data) {
@@ -285,7 +292,7 @@ function initMap(zoom = 4, showMarker = true) {
   
 
   if (showMarker) {
-    addMarker(mapCenter, myIcon, data_0.comment);
+    addMarker(mapCenter, myIconStock, data_0.comment);
   }
 }
 
